1e651a3579aaa107812c99e5474c9de71163efbd
Elliot Wright <wright.elliot@gmail.com>
2015-12-24 09:41:27 +0000

    Added missing license file header and author.

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -1,11 +1,23 @@
 <?php
 
+/**
+ * This file is part of the "byng/pimcore-elasticsearch-plugin" project.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the LICENSE is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Pimcore\Controller\Action\Admin as AdminAction;
 
 /**
  * Elasticsearch_IndexController
  *
  * @author Elliot Wright <elliot@elliotwright.co>
+ * @author Matt Ward <matt@byng.co>
  */
 class Elasticsearch_IndexController extends AdminAction
 {
